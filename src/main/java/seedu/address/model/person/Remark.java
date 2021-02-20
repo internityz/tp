5c89b51025808daaa4dc90989481e1c66bdce9d0
Zong Yu <hanzongyu@live.com>
2021-02-21 02:46:50 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Represents a Person's remark in the address book.
+     * Guarantees: immutable; is always valid
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
